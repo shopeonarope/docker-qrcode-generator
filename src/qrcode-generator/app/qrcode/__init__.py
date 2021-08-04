2279c0f5316bb34cfdda285541ccd6941e4b9074
Kyle Brown <kyle.brown@collectivemedicaltech.com>
2021-08-04 08:26:23 -0600

    guest wifi template

--- src/qrcode-generator/app/qrcode/__init__.py
+++ src/qrcode-generator/app/qrcode/__init__.py
@@ -1,10 +1,15 @@
-from flask import Flask, Blueprint, request, send_file
+import base64
+from flask import Flask, Blueprint, request, render_template
 from flask_qrcode import QRcode
+from pathlib import Path
 
 qrcode = Blueprint('qrcode', __name__)
 
 generate_qrcode = QRcode()
 
-@qrcode.route('/qrcode/<data>', methods=['GET'])
-def get_qrcode(data):
-    return send_file(generate_qrcode(data, error_correction='H', mode='raw'), mimetype='image/png')
+@qrcode.route('/', methods=['GET'])
+def get_qrcode():
+    data = Path('/etc/convert_to_qr').read_text()
+    image_bytes = generate_qrcode(data, error_correction='H', mode='raw')
+    image_b64=(base64.b64encode(image_bytes.read())).decode()
+    return render_template('index.html',image_b64=image_b64,password=data)
--- src/qrcode-generator/main.py
+++ src/qrcode-generator/main.py
@@ -4,10 +4,11 @@ from app import create_app
 
 if __name__ == '__main__':
     HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
+    DEBUG = os.environ.get('SERVER_DEBUG', False)
     try:
         PORT = int(os.environ.get('SERVER_PORT', '8000'))
     except ValueError:
         PORT = 8000
 
     application = create_app()
-    application.run(HOST, PORT, debug=True)
+    application.run(HOST, PORT, debug=DEBUG)
